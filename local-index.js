a5b3cc3632ec26d2f83344c180c29362ed45321b
Sarthak Gupta <16374350+sarthakgup@users.noreply.github.com>
2020-10-25 00:08:49 -0700

    updated buttons and added some comments

--- local-index.js
+++ local-index.js
@@ -1,18 +1,19 @@
-const dogs = [
-    { name: 'Sammy' },
-    { name: 'Roscoe' },
-    { name: 'Artichoke' },
-    { name: 'Pookie' },
+const instructions = 
+[
+    { name: 'Project By: Sarthak Gupta' },
+    { name: '\nThe goal is to leave you with helpful resources to stay up to date with COVID Developments' },
+    { name: '\nClick on the different boxes to explore cool resources such as stats, news, and even mask 3D printing tips!' },
+    { name: '\nEnjoy my first web app ever which I built using html and javascript and deployed on the incredible Azure \n' },
 ]
 
 
 function displayDogs()
 {
-    for (let dog of dogs)
+    for (let instruction of instructions)
     {
         const dogUI = document.createElement('li');
         dogUI.className = 'list-group-item';
-        dogUI.innerText = dog.name;
+        dogUI.innerText = instruction.name;
 
         document.getElementById('dogs-list').appendChild(dogUI);
     }
