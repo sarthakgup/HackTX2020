7794112bbb3f36523bb0f9ef0d6ea1628626f25e
Sarthak Gupta <16374350+sarthakgup@users.noreply.github.com>
2020-10-25 03:31:35 -0700

    Adding pages and tried creating color-changing bg

--- local-index.js
+++ local-index.js
@@ -2,8 +2,8 @@ const instructions =
 [
     { name: 'Project By: Sarthak Gupta' },
     { name: '\nThe goal is to leave you with helpful resources to stay up to date with COVID Developments' },
-    { name: '\nClick on the different boxes to explore cool resources such as stats, news, and even mask 3D printing tips!' },
-    { name: '\nEnjoy my first web app ever which I built using html and javascript and deployed on the incredible Azure \n' },
+    { name: '\nClick on the different boxes to explore cool resources such as stats, news, and safety tips!' },
+    { name: '\nEnjoy my first web app ever which I built using html and javascript and deployed using Azure \n' },
 ]
 
 //creating method displayInstructions to run a for each loop and run through and display each element in 'instructions'
@@ -21,6 +21,8 @@ function displayInstructions()
 
 async function main()
 {
+    
+
     //Calling displayInstructions method
     displayInstructions();
 }
